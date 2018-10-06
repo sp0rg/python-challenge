cf3c00b84871e190b1488056620c3595070c4be4
Andy H <ahumburg@gmail.com>
2018-10-05 19:58:25 -0500

    loading up for submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,10 @@
 import os
 import csv
 
+#defines input path
 budget_data_csv = os.path.join("..","Resources", "budget_data.csv")
+#defines output path
+output_data_csv = os.path.join("pyBank_output.csv")
 
 #define two empty lists for the months and revenue
 #bankMonths - list for each month/year combo
@@ -31,6 +34,7 @@ totalMonths = len(bankMonths)
 #print(bankMonths,bankRevenue) #just to validate that
 #this hot mess is actually kind of working
 
+##### Finding the Net revenue
 netPL = 0
 #for y in range(len(bankRevenue)):
 #    netPL.append(y)
@@ -42,7 +46,7 @@ netPL = 0
 for r in range(len(bankRevenue)):
     netPL += bankRevenue[r]
 
-#increase/decreast most-est variables
+###### Finding increase/decreast most-est
 incMost = bankRevenue[0]
 decMost = bankRevenue[0]
 monthInc = bankMonths[0]
@@ -60,71 +64,60 @@ for r in range(len(bankRevenue)):
     elif bankRevenue[r]  - bankRevenue[r - 1] <= decMost:
         decMost = bankRevenue[r] - bankRevenue[r - 1]
         monthDec = bankMonths[r]
-#print(incMost,monthInc) #test results - rev is off
-#print(decMost,monthDec) #test results - rev is off
 
-#print(netPL)
-#print(totalMonths)   #test the total - it works 
-#header = next(budget_data_csv)
-#Your task is to create a Python script that analyzes the records to calculate each of 
-# the following:
-#def getBankTotal(BankData):
-  #* The total number of months included in the dataset
-    #totalMonths = []#(BankData[0])
-    #for x in(BankData[0]):
-        #if x not in totalMonths:#.count(x):
-            #totalMonths.append(x)
-    #return totalMonths
-    #print(len(totalMonths)-1)
-    
-    #for months in totalMonths:
-     #   print(months)
-    #print (x)
-    #print totalMonths
-    #totalMonths = (BankData[0])
- # * The total net amount of "Profit/Losses" over the entire period
-    #netPL = []
-    #for y in (BankData[1]):
-        #netPL.append(y)
-    #return netPL
-    #print(int(netPL)
- 
- # * The average change in "Profit/Losses" between months over the entire period
-#avgChange = round(netPL/int(totalMonths), 2)
-#maybe need to do something nested?
-#like variance = value[r+1]-value[r]
-#or take evens minus odds? and add the differences together?
-#then take that and divide by months
-#for r in range(len(bankRevenue)):
-    #if bankRevenue[r] >= incMost:
-        #incMost = bankRevenue[r]
-        #monthInc = bankMonths[r]
-    #elif bankRevenue[r] <= decMost:
-        #decMost = bankRevenue[r]
-        #monthDec = bankMonths[r]
-#print(avgChange) #doesn't match given output
- 
- # * The greatest increase in profits (date and amount) over the entire period
+###### AVERAGE SECTION
+avgrevChange = []
+moreAvg = 0
+somewhatAvg = 0
+bobeatsBroccoli = 0 #there are only so many ways to say average
+removeCarrots = 0 #tired of broccoli? bring on the carrots!
 
- # * The greatest decrease in losses (date and amount) over the entire period
+#calculation to add each iteration of difference
+for r in range(len(bankRevenue)): 
+    if avgrevChange != 0:
+        bobeatsBroccoli =  bankRevenue[r] - bankRevenue[r - 1]
+        avgrevChange.append(bobeatsBroccoli)
 
-#* As an example, your analysis should look similar to the one below:
+#kept coming up with zero
+#assumed that subtracting first row from last row it would throw the sum off later
+#dug around on the interwebs a bit, but it makes sense
+#zero indexed takes the first row minus the very last row (-1)
+removeCarrots = bankRevenue[0] - bankRevenue[-1]
+
+#This gets our net - still need to remove those carrots
+for r in range(len(avgrevChange)):
+    moreAvg += avgrevChange[r]
+
+#takes removes the carrots to get a proper net value
+evenmoreAvg = moreAvg - removeCarrots
+
+#divide for average, then round and all that jazz
+#had to account that there really should only be 85 pairs of differences
+#when we removed carrots we took the list from 86 to 85 :)
+somewhatAvg = evenmoreAvg / (totalMonths - 1)
+somewhatRnd = str(round(somewhatAvg,2))
+
+###### Print Section
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {totalMonths}")
 print(f"Total: ${netPL}")
-#print(f"Average Change: ${avgChange}")
+print(f"Average Change: ${somewhatRnd}")
 print(f"Greatest Increase in Profits: {monthInc} (${incMost})")
 print(f"Greatest Decrease in Profits: {monthDec} (${decMost})")
- # text
- # Financial Analysis
- # ----------------------------
- # Total Months: 86
- # Total: $38382578
- # Average  Change: $-2315.12
- # Greatest Increase in Profits: Feb-2012 ($1926159)
- # Greatest Decrease in Profits: Sep-2013 ($-2196167)
+ 
+with open(output_data_csv, mode='w', newline='') as summary_txt:
+    writer = csv.writer(summary_txt)
 
+    writer.writerows([
+        ["Financial Analysis"],
+        ['-' * 28],
+        [f"Total Months: {totalMonths}"],
+        [f"Total: ${netPL}"],
+        [f"Average Change: ${somewhatRnd}"],
+        [f"Greatest Increase in Profits: {monthInc} (${incMost})"],
+        [f"Greatest Decrease in Profits: {monthDec} (${decMost})"]
+    ])
 
     #* In addition, your final script should both print the analysis to the terminal 
     ## and export a text file with the results.
